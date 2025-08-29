45d9071768d704f876ce1263295b714715f5e327
Adiba-hossain <21adibahossain@gmail.com>
2025-08-29 17:12:46 +0600

    heart count complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+// Heart Icons Click Event
+const hearts = document.getElementsByClassName("fa-heart");
+const heartCount = document.getElementById("heart-count");
+let heartCountNumber = parseInt(heartCount.innerText);
+for (const heart of hearts) {
+  heart.addEventListener("click", function () {
+    heartCountNumber += 1;
+    heartCount.innerText = heartCountNumber;
+  });
+}
